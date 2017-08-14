e3ffcc19505e1c303c1f666270d355764bdc61b7
charles <charlesipn@gmail.com>
2017-08-14 00:41:53 -0500

    cover first version

--- src/js/config/index.js
+++ src/js/config/index.js
@@ -1,3 +1,8 @@
 let exp = module.exports = {
-  particlesConf: 'https://s3-us-west-2.amazonaws.com/devcharles/web/personal-page/particlesjs-devcharles-config.json'
+  particlesConf: 'https://s3-us-west-2.amazonaws.com/devcharles/web/personal-page/particlesjs-devcharles-config.json',
+  apiGHInfoProfileUrl: 'https://api.github.com/users/devCharles',
+  fbWhite: 'https://s3-us-west-2.amazonaws.com/devcharles/cdn/social/fb_white.png',
+  fbBlue: 'https://s3-us-west-2.amazonaws.com/devcharles/cdn/social/fb_blue.png',
+  twWhite: 'https://s3-us-west-2.amazonaws.com/devcharles/cdn/social/tw_white.png',
+  twBlue: 'https://s3-us-west-2.amazonaws.com/devcharles/cdn/social/tw_blue.png'
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,24 @@
+import 'babel-polyfill'
 import '../scss/index.scss'
 import config from './config'
 import cover from '../html/cover.html'
 
-
 document.getElementById("cover").innerHTML = cover
-console.log(config.particlesConf);
+
+const getGHProfileInfo = async () => await (await fetch(config.apiGHInfoProfileUrl)).json()
+let GHProfileInfo;
+getGHProfileInfo().then((GHResponse)=>{
+  console.log("GH Profile information obtained");
+  document.getElementById("profile-pic").src = GHResponse.avatar_url
+}).catch(()=>{
+  console.error("can not get GH profile information from '"+ config.apiGHInfoProfileUrl +"'");
+})
+
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
 particlesJS.load('particles-js', config.particlesConf, function() {
   console.log('callback - particles.js config loaded');
 });
+
+//set social media logos
+document.getElementById("fb-logo").src = config.fbWhite
+document.getElementById("tw-logo").src = config.twWhite
