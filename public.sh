47ee790cd813d171bb33d2728ce0e20cb36d0fad
devcharles <charlesipn@gmail.com>
2017-09-23 22:37:44 -0500

    favicon added

--- public.sh
+++ public.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-echo "deleteing dist..."
+echo " > deleteing dist..."
 rm -rf dist
-echo "running webpack (webpack.config.js)"
+echo " > running webpack (webpack.config.js)"
 webpack --config webpack.config.js
-echo "copying index.html to dist"
+echo " > copying index.html to dist"
 cp index.html dist
-echo "copying README.md to dist"
+echo " > copying README.md to dist"
 cp README.md dist
-echo "deleting charlesTemp"
+echo " > deleting charlesTemp"
 rm -rf ../charlesTemp
-echo "creating charlesTemp"
+echo " > creating charlesTemp"
 mkdir ../charlesTemp
-echo "copying dist in charlesTemp"
+echo " > copying dist in charlesTemp"
 cp dist/* ../charlesTemp
-echo "checking out to master branch"
+echo " > checking out to master branch"
 git checkout master
-echo "deleting exisitng index.html & bunle.js and README.md"
+echo " > deleting exisitng index.html & bunle.js and README.md"
 rm -f index.html bundle.js README.md
-echo "moving charlesTemp content to base folder"
+echo " > moving charlesTemp content to base folder"
 mv ../charlesTemp/* .
-echo "deleting charlesTemp"
+echo " > deleting charlesTemp"
 rm -rf ../charlesTemp
-echo 'DONE!'
+echo ' > DONE!'
 
 # "up": "rm -rf dist && webpack --config webpack.config.js && cp index.html dist && cp README.md dist && rm -rf ../charlesTemp && mkdir ../charlesTemp && cp dist/* ../charlesTemp && git checkout master && rm -f index.html bundle.js README.md && mv ../charlesTemp/* . && rm -rf ../charlesTemp && echo 'DONE!'"
