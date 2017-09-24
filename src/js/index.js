07aba49f994a84ec273e799126a7308a33a11e18
devcharles <charlesipn@gmail.com>
2017-09-24 18:44:52 -0500

    social links added

--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,38 @@
 import 'babel-polyfill'
 import '../scss/index.scss'
 import config from './config'
-import cover from '../html/cover.html'
 
-document.getElementById("cover").innerHTML = cover
 
 const getGHProfileInfo = async () => await (await fetch(config.apiGHInfoProfileUrl)).json()
 let GHProfileInfo;
 getGHProfileInfo().then((GHResponse)=>{
-  console.log("GH Profile information obtained");
-  document.getElementById("profile-pic").src = GHResponse.avatar_url
   console.log("<< IN CODE WE TRUST >>")
+  // console.info("GH Profile information obtained");
+  document.getElementById("profile-pic").src = GHResponse.avatar_url
+
 }).catch(()=>{
-  console.error("Can't get  GH profile information from '"+ config.apiGHInfoProfileUrl +"'");
+  console.error(`Can't get GH profile information from '${config.apiGHInfoProfileUrl}'`);
 })
 
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
 particlesJS.load('particles-js', config.particlesConf, function() {
-  console.log('callback - particles.js config loaded');
+  // console.log('callback - particles.js config loaded');
 });
 
-//set social media logos
-document.getElementById("fb-logo").src = config.fbWhite
-document.getElementById("tw-logo").src = config.twWhite
+//social media
+const fbLogo = document.getElementById("fb-logo")
+const twLogo = document.getElementById("tw-logo")
+const ghLogo = document.getElementById("gh-logo")
+
+//set social media links
+fbLogo.addEventListener("click", () => {
+  window.open("http://facebook.com/YAYOSILVAIPN","_blank")
+})
+
+twLogo.addEventListener("click", () => {
+  window.open("http://twitter.com/devcharles","_blank")
+})
+
+twLogo.addEventListener("click", () => {
+  window.open("https://github.com/devCharles","_blank")
+})
